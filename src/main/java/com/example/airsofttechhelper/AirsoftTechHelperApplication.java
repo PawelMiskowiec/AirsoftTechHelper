61fa7761d8141c3eef81b83046689e28552807c4
Pawel Miskowiec <pmiskowi@student.wszib.edu.pl>
2022-06-21 18:42:06 +0200

    first commit

---
+++ src/main/java/com/example/airsofttechhelper/AirsoftTechHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.airsofttechhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirsoftTechHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirsoftTechHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/airsofttechhelper/AirsoftTechHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airsofttechhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirsoftTechHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
