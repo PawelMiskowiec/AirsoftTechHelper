8b5ed9dbcbf5fc385a399a50e14fbb0e3c13edbf
Pawel Miskowiec <pmiskowi@student.wszib.edu.pl>
2022-08-10 17:33:51 +0200

    add spring-boot-configuration-processor dependency, change apllication.properties format to yaml

--- src/main/java/com/example/airsofttechhelper/AirsoftTechHelperApplication.java
+++ src/main/java/com/example/airsofttechhelper/AirsoftTechHelperApplication.java
@@ -3,6 +3,7 @@ package com.example.airsofttechhelper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class AirsoftTechHelperApplication {
 
     public static void main(String[] args) {
